ad32fbe1a45bc7c9dd2fd65559e14ff11243463d
vikasprajapati1812 <118847227+vikasprajapati1812@users.noreply.github.com>
2024-09-05 21:46:34 +0530

    Delete Rick&Morty_Characters directory

--- Rick&Morty_Characters/src/Components/DateOfCharacterCreated.js
+++
@@ -1,7 +0,0 @@
-const DateOfCharacterCreated = ({ created }) => {
-  const characterDate = new Date(created);
-  const currentDate = new Date();
-  const createdYear = currentDate.getFullYear() - characterDate.getFullYear();
-  return <span>Created {createdYear} years ago</span>;
-};
-export default DateOfCharacterCreated;
